44039b574bc4d07c39d0a3fcdaba77f6898e18eb
Tushar Mathur <tusharmath@gmail.com>
2016-01-28 18:29:18 +0530

    feat(index): pre-init the decorators with the action bus for communication

--- index.js
+++ index.js
@@ -3,3 +3,18 @@
  */
 
 'use strict'
+const ReactDOM = require('react-dom')
+const create = require('./src/create')
+const decorators = require('./src/decorators')
+
+module.exports = () => {
+  const actionBus = create()
+  const observer = actionBus.observer
+  return {
+    draggable: decorators.draggable({ReactDOM, observer}),
+    droppable: decorators.droppable({ReactDOM, observer}),
+    getStream: actionBus.getStream
+  }
+}
+module.exports.create = create
+module.exports.decorators = decorators
