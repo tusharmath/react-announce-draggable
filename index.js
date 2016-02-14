ba62abe761187420f29346303d4f2ce9e8653027
Tushar Mathur <tusharmath@gmail.com>
2016-02-15 02:36:05 +0530

    feat(consolidate): use consolidate() instead of create
    use consolidate function to create a unified stream for drag and drop events
    
    BREAKING CHANGE: create() method is removed

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const ReactDOM = require('react-dom')
 const create = require('./src/create')
 const decorators = require('./src/decorators')
 
-module.exports = () => {
-  const actionBus = create()
-  const observer = actionBus.observer
-  return {
-    draggable: decorators.draggable({ReactDOM, observer}),
-    droppable: decorators.droppable({ReactDOM, observer}),
-    stream: actionBus.getStream()
-  }
-}
-module.exports.create = create
-module.exports.decorators = decorators
+exports.consolidate = create
+exports.draggable = decorators(ReactDOM)
+exports.droppable = decorators(ReactDOM)
