d60f5936647ff0804f1c52fa29b8cbe106cf67a4
Tushar Mathur <tusharmath@gmail.com>
2016-02-15 01:10:00 +0530

    refactor(decorators): use common _findDomNode method

--- src/decorators.js
+++ src/decorators.js
@@ -4,26 +4,29 @@
 
 'use strict'
 
+const ReactDOM = require('react-dom')
+const e = exports
 const createDeclarative = require('react-announce').createDeclarative
-
 const dispatch = (observer, component, type, event) => observer
-  .onNext({event, component, type})
+    .onNext({event, component, type})
 
-exports.draggable = createDeclarative(function (stream, dispose, u) {
-  dispose(
-    stream
-      .filter(x => x.event === 'DID_MOUNT')
-      .map(() => u.ReactDOM.findDOMNode(this))
-      .subscribe(el => el.addEventListener('dragstart', x => dispatch(u.observer, this, 'DRAG_START', x)))
+e.ReactDOM = ReactDOM
+e._findDomNode = (ReactDOM, stream) => stream
+    .filter(x => x.event === 'DID_MOUNT')
+    .map(() => ReactDOM.findDOMNode(this))
+
+e.draggable = createDeclarative(function (stream, dispose, u) {
+  dispose(e
+    ._findDomNode(u.ReactDOM, stream)
+    .subscribe(el => el.addEventListener('dragstart', x => dispatch(u.observer, this, 'DRAG_START', x)))
   )
 })
 
-exports.droppable = createDeclarative(function (stream, dispose, u) {
-  dispose(
-    stream.filter(x => x.event === 'DID_MOUNT')
-      .map(() => u.ReactDOM.findDOMNode(this))
-      .subscribe(el => {
-        el.addEventListener('dragover', x => dispatch(u.observer, this, 'DRAG_OVER', x))
-        el.addEventListener('drop', x => dispatch(u.observer, this, 'DROP', x))
-      }))
+e.droppable = createDeclarative(function (stream, dispose, u) {
+  dispose(e
+    ._findDomNode(u.ReactDOM, stream)
+    .subscribe(el => {
+      el.addEventListener('dragover', x => dispatch(u.observer, this, 'DRAG_OVER', x))
+      el.addEventListener('drop', x => dispatch(u.observer, this, 'DROP', x))
+    }))
 })
